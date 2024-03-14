f894ce49e3e1b17cba415ce3e78a91cd6a9a1bac
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-03-14 14:59:42 +0000

    Add files via upload

---
+++ 4_11_rectangle_intersection.py
@@ -0,0 +1,35 @@
+
+import collections
+
+
+Rect = collections.namedtuple('Rect', ('x', 'y', 'width', 'height'))
+
+
+def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
+    def is_intersect(r1, r2):
+        return (r1.x <= r2.x + r2.width and r1.x + r1.width >= r2.x
+                and r1.y <= r2.y + r2.height and r1.y + r1.height >= r2.y)
+
+    if not is_intersect(r1, r2):
+        return Rect(0, 0, -1, -1)  # No intersection.
+    return Rect(max(r1.x, r2.x), max(r1.y, r2.y),
+                min(r1.x + r1.width, r2.x + r2.width) - max(r1.x, r2.x),
+                min(r1.y + r1.height, r2.y + r2.height) - max(r1.y, r2.y))
+
+
+def intersect_rectangle_wrapper(r1, r2):
+    return intersect_rectangle(Rect(*r1), Rect(*r2))
+
+
+def res_printer(prop, value):
+    def fmt(x):
+        return [x[0], x[1], x[2], x[3]] if x else None
+
+    if prop in (PropertyName.EXPECTED, PropertyName.RESULT):
+        return fmt(value)
+    else:
+        return value
+
+
+
+

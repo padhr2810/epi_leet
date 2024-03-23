6bcd0d9f08478430098b500cc312a5d9e0e6edaa
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-03-23 12:48:56 +0000

    Add files via upload

---
+++ epi_py/tests/4_1 parity.py
@@ -0,0 +1,15 @@
+from test_framework import generic_test
+
+
+def parity(x: int) -> int:
+    # TODO - you fill in here.
+    result = 0
+    while x:
+        result ^= (x & 1)
+        x >>= 1
+        
+    return result
+
+
+if __name__ == '__main__':
+    exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
---
+++ epi_py/tests/4_2_swap_bits.py
@@ -0,0 +1,16 @@
+from test_framework import generic_test
+
+
+def swap_bits(x, i, j):
+    # TODO - you fill in here.
+    if ((x >> i) & 1) != ((x >> j) & 1):
+        bitmask = 1 << i | 1 << j
+        x ^= bitmask
+    
+    return x
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('swap_bits.py', 'swap_bits.tsv',
+                                       swap_bits))

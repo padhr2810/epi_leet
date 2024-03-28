b28fa853d7c785f347df050960e4dab8de9ab752
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-03-28 22:34:28 +0000

    Add files via upload

--- leetcode_75/18_find_highest_altitude.py
+++ leetcode_75/18_find_highest_altitude.py
@@ -1,23 +1,14 @@
 
 """
-There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+There is a biker going on a road trip. 
+The road trip consists of n + 1 points at different altitudes. 
+The biker starts his trip on point 0 with altitude equal 0.
 
-You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
-
- 
-
-Example 1:
-
-Input: gain = [-5,1,5,0,-7]
-Output: 1
-Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
-Example 2:
-
-Input: gain = [-4,-3,-2,-1,4,3,2]
-Output: 0
-Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+You are given an integer array gain of length n where gain[i] is the net gain in 
+altitude between points i and i + 1 for all (0 <= i < n). 
+Return the highest altitude of a point.
 """
-
+import itertools 
 class Solution(object):
     def largestAltitude(self, gain):
         """
@@ -32,5 +23,11 @@ class Solution(object):
             if current > highest:
                 highest = current
         return highest
-        ## return max(accumulate(gain, initial=0))  # MORE COMPACT CODE...
-    
+        ## return max(itertools.accumulate(gain, initial=0))  # MORE COMPACT CODE...
+
+soln = Solution()
+assert soln.largestAltitude(gain = [-5,1,5,0,-7]) == 1
+# Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+assert soln.largestAltitude(gain = [-4,-3,-2,-1,4,3,2]) == 0
+# Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.

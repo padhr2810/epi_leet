4d40bfe25031b86dfc03fdb4776d9b37d3867ab4
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-06-06 17:13:58 +0100

    Add files via upload

--- epi_py/ch15_recursion/15_1_hanoi.py
+++ epi_py/ch15_recursion/15_1_hanoi.py
@@ -2,6 +2,8 @@
 import functools
 from typing import List
 
+import pprint
+
 #from test_framework import generic_test
 #from test_framework.test_failure import TestFailure
 #from test_framework.test_utils import enable_executor_hook
@@ -9,15 +11,16 @@ from typing import List
 NUM_PEGS = 3
 
 record_of_recursion_calls= []
+
 def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
     def compute_tower_hanoi_steps(num_rings_to_move, from_peg, to_peg,
                                   use_peg):
+                                  
         if num_rings_to_move > 0:
             print(f"\n\n\n#####\nStarting FIRST internal call to recursion !!!!!!!!!!!!!!!!!!!!!!!!!")
             print(f"\npegs = {pegs}")
             print(f"\nNum rings to move = {num_rings_to_move}")
-            print(f"\nfrom_peg = {from_peg}; to_peg = {to_peg}; use_peg = {use_peg}")
-            record_of_recursion_calls.append(f"FIRST: {num_rings_to_move}")
+            record_of_recursion_calls.append(f"FIRST: {num_rings_to_move}; pegs = {pegs};  Peg order in func = {from_peg}; {use_peg}; {to_peg}")
             
             
             compute_tower_hanoi_steps(num_rings_to_move - 1, from_peg, use_peg,
@@ -29,7 +32,7 @@ def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
             print(f"\npegs = {pegs}")
             print(f"\nNum rings to move = {num_rings_to_move}")
             print(f"\nfrom_peg = {from_peg}; to_peg = {to_peg}; use_peg = {use_peg}")
-            record_of_recursion_calls.append(f"SECOND: {num_rings_to_move}")
+            record_of_recursion_calls.append(f"SECOND: {num_rings_to_move}; pegs = {pegs};  Peg order in func = {use_peg}; {to_peg}; {from_peg}")
 
             compute_tower_hanoi_steps(num_rings_to_move - 1, use_peg, to_peg,
                                       from_peg)
@@ -39,7 +42,8 @@ def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
     pegs = [list(reversed(range(1, num_rings + 1)))
             ] + [[] for _ in range(1, NUM_PEGS)]
     compute_tower_hanoi_steps(num_rings, 0, 1, 2)
-    print(f"\nNumber of recursion calls = {record_of_recursion_calls}")
+    print(f"\nNumber of recursion calls =")
+    pprint.pp(record_of_recursion_calls)
     print(f"\nresult = {result}")
     return result
 

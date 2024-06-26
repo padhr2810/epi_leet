cfaf7eb5aa67d6091014a400a4cc2e5c9f38a8a6
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-06-26 08:31:44 +0100

    Add files via upload

--- epi_py/ch15_recursion/15_1_hanoi.py
+++ epi_py/ch15_recursion/15_1_hanoi.py
@@ -4,6 +4,19 @@ TRICK: ONLY REACHES THE SECOND CALL OF HELPER FUNCTION IF "num_rings_to_move == 
 INPUT TO HELPER FUNC = "from_peg, to_peg, use_peg" 
     ...BUT IN FIRST RECURSION IT'S "from_peg, use_peg, to_peg" ...I.E.JUST 2ND & 3RD CHANGE.
     ...BUT IN SECOND RECURSION IT'S "use_peg, to_peg, from_peg" ... I.E. FIRST ONE CHANGES.
+ANYTIME YOU EXIT THE "FIRST" RECURSION, A MOVE OCCURS.
+
+To move N disks,
+
+1. First you move N-1 disks to the intermediate position,
+2. Then you move the bottom disk to the destination,
+3. Finally you move the N-1 disks from the intermediate position to the destination.
+The code mimics that.
+
+The only thing to understand is that the "roles" of source, destination and temporary are different for the sub-towers.
+-- When I say "move N-1 from source to temp", it means source2 = source, dest2 = temp, and as a consequence temp2 = dest.
+-- When you move the bottom disk, all is unchanged ( source3 = source, dest3 = dest, temp3 = temp
+-- When I say "move N-1 from temp to dest", it means source4 = temp, dest4 = dest, and as a consequence temp4 = source.
 """
 
 import functools
@@ -37,21 +50,31 @@ def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
             
             compute_tower_hanoi_steps(num_rings_to_move - 1, from_peg, use_peg,
                                       to_peg)
+                                      
+            record_of_recursion_calls.append("")
+            record_of_recursion_calls.append("       ***** EXIT FIRST")
+            record_of_recursion_calls.append("")
+            
             #### MOVE OCCURS HERE!!!
             pegs[to_peg].append(pegs[from_peg].pop())
             result.append([from_peg, to_peg])
-            record_of_recursion_calls.append(f"   *** WHEN MAKE THE MOVE -- from_peg={from_peg}; to_peg={to_peg}")
+            record_of_recursion_calls.append(f"   *** MAKE THE MOVE -- from_peg={from_peg}; to_peg={to_peg}")
+            record_of_recursion_calls.append("")
             
             print(f"\n\n\n#####\n#####\n#####\nStarting SECOND internal call to recursion")
-            record_of_recursion_calls.append(f"SECOND: num_rings_to_move before SECOND = {num_rings_to_move}; pegs = {pegs};  Peg order in func = from_peg={use_peg}; to_peg={to_peg}; use_peg{from_peg}; RESULT = {result}")
+            record_of_recursion_calls.append(f"SECOND: num_rings_to_move before SECOND = {num_rings_to_move}; pegs = {pegs};  Peg order in func = from_peg=use_peg/{use_peg}; to_peg=to_peg/{to_peg}; use_peg=from_peg/{from_peg}; RESULT = {result}")
             SECOND_FUNC_COUNTER+= 1
             print(f"SECOND_FUNC_COUNTER = {SECOND_FUNC_COUNTER}")
 
             compute_tower_hanoi_steps(num_rings_to_move - 1, use_peg, to_peg,
                                       from_peg)
+            record_of_recursion_calls.append("")
+            record_of_recursion_calls.append("       ***** EXIT SECOND")
+            record_of_recursion_calls.append("")
                                       
         else:
-            record_of_recursion_calls.append("                    ********** num_rings_to_move == 0")
+            record_of_recursion_calls.append("                    ********** EXITED A RECURSION --- num_rings_to_move == 0")
+            record_of_recursion_calls.append("")
             IF_ZERO_TRUE_COUNTER += 1
             print(f"\n\nIF_ZERO_TRUE_COUNTER = {IF_ZERO_TRUE_COUNTER}")
             print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")

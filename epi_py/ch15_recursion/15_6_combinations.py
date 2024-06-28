48ebcac03d25184fb5a30f03ef4d1e51837782d5
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-06-28 19:16:03 +0100

    Update 15_6_combinations.py

--- epi_py/ch15_recursion/15_6_combinations.py
+++ epi_py/ch15_recursion/15_6_combinations.py
@@ -1,3 +1,34 @@
+
+
+############################ GEEKS FOR GEEKS: - https://www.geeksforgeeks.org/combinations-in-python-without-using-itertools/
+
+# Function to create combinations 
+# without itertools
+def n_length_combo(lst, n):
+     
+    if n == 0:
+        return [[]]
+     
+    l =[]
+    for i in range(0, len(lst)):
+         
+        m = lst[i]
+        remLst = lst[i + 1:]
+         
+        remainlst_combo = n_length_combo(remLst, n-1)
+        for p in remainlst_combo:
+             l.append([m, *p])
+           
+    return l
+ 
+# Driver code
+if __name__=="__main__":
+    arr ="abc"
+    print(n_length_combo([x for x in arr], 2))
+
+
+
+############################ EPI
 from typing import List
 
 from test_framework import generic_test, test_utils

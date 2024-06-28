5436d3e1208448f48f086793acaf67a9fe06485c
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-06-28 19:14:27 +0100

    Update 15_4_permutations.py

--- epi_py/ch15_recursion/15_4_permutations.py
+++ epi_py/ch15_recursion/15_4_permutations.py
@@ -1,3 +1,51 @@
+
+################# GEEKS FOR GEEKS. -- https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
+
+
+# RECURSIVE - PERMUTATIONS - Print permutations of a given list
+def permutation(L1):
+
+    # EDGE CASE: If L1 is empty then there are no permutations
+    if len(L1) == 0:
+        return []
+ 
+    # BASE CASE: If there is only one element in L1 then, only
+    # one permutation is possible
+    if len(L1) == 1:
+        return [L1]
+ 
+    # RECURSIVE CASE = INSIDE 2 LOOPS.
+    # Find the permutations for L1 if > 1 chars.
+
+    current_perm = [] # empty list that will store current permutation
+ 
+    # Iterate the input(L1) and calculate the permutation
+    for i in range(len(L1)):
+       this_one = L1[i]
+ 
+       # Extract L1[i] or m from the list.  remLst is
+       # remaining list
+       remLst = L1[:i] + L1[i+1:]
+ 
+       # Generating all permutations where m is first
+       # element
+       for p in permutation(remLst):
+           current_perm.append([this_one] + p)
+    return current_perm
+ 
+ 
+# Driver program to test above function
+inputs = list('123')
+result = permutation(inputs)
+for p in result:
+    print (p)
+print(f"Num perms = {len(result)}")
+
+
+
+
+################# EPI 
+
 from typing import List
 
 from test_framework import generic_test, test_utils

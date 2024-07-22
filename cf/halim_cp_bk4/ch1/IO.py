c11f467f50ebabd2beb151ef1b88aea37b99327b
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-07-22 10:48:36 +0100

    Update IO.py

--- cf/halim_cp_bk4/ch1/IO.py
+++ cf/halim_cp_bk4/ch1/IO.py
@@ -5,6 +5,13 @@
 # sys.stdin.readline()  --  The readline() also takes input from the user but also reads the escape character.
 
 # IO_in1.txt
+"""
+3
+1 2
+5 7
+6 3
+"""
+
 import sys
 inputs = iter(sys.stdin.readlines())
 TC = int(next(inputs))
@@ -12,12 +19,26 @@ for _ in range(TC):
     print(sum(map(int, next(inputs).split())))
 
 # # IO_in2.txt
+"""
+1 2
+5 7
+6 3
+0 0
+1 1
+"""
+
 # import sys
 # for line in sys.stdin.readlines():
 #     if line == '0 0\n': break
 #     print(sum(map(int, line.split())))
 
 # # IO_in3.txt
+"""
+1 2
+5 7
+6 3
+"""
+
 # import sys
 # for line in sys.stdin.readlines():
 #     print(sum(map(int, line.split())))
@@ -34,11 +55,26 @@ for _ in range(TC):
 #     print("Case %s: %s" % (c, sum(map(int, line.split()))))
 
 # # IO_in4.txt
+"""
+1 1
+2 3 4
+3 8 1 1
+4 7 2 9 3
+5 1 1 1 1 1
+"""
 # import sys
 # for line in sys.stdin.readlines():
 #     print(sum(map(int, line.split()[1:]))) # skip the first integer
 
 # # IO_in5.txt
+"""
+1
+3 4
+8 1 1
+7 2 9 3
+1 1 1 1 1
+"""
+
 # import sys
 # for line in sys.stdin.readlines():
 #     print(sum(map(int, line.split())))
